9b1c941127f03d1668b44c06016405dc1a52a92e
AnnaNikolova05 <120709168+AnnaNikolova05@users.noreply.github.com>
2023-06-14 08:32:30 +0300

    Add files via upload

---
+++ Controller/AnimalLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha6.Controller
+{
+    internal class AnimalLogic
+    {
+        internal void Create(Animal newAnimal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
